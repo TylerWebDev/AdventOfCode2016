55141bd6c40f8ddd80e1effc70f2014e197f56c7
Bryan Cross <Bryan Cross>
2016-12-07 20:01:20 -0600

    Start...done?

--- Day1/C#/AoC_Day1/Assets/000 - MyAssets/Scripts/NavScriptMain.cs
+++ Day1/C#/AoC_Day1/Assets/000 - MyAssets/Scripts/NavScriptMain.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NavScriptMain : MonoBehaviour
 {
-
-
+	private string heading;
+	private int blocks;
+	private List<Direction> directions;
 	// Use this for initialization
 	void Start () {
-		
+		var file = File.ReadAllText("..\..\..\input.txt"));
+		var directionsString = Regex.Replace(file,@"\s+","").Split(',')
+		directions = directionsString.Select(d => new Direction(d.SubString(0,1),d.SubString(1)));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+}
+
+private class Direction
+{
+	public string Turn {get;set;}
+	public int Steps {get;set;}
 }
